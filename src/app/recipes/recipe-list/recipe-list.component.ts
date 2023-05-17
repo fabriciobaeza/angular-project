9a2c23f9bb05172f90627fa12e7f804d203ead4d
Fabricio Baeza <fabriciobaeza@gmail.com>
2023-05-16 23:00:21 -0400

    Components & Data Biding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe.model'
 
 @Component({
@@ -7,8 +7,14 @@ import {Recipe} from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[]=[
     new Recipe('A test recipe', 'this is a test', 'https://minimalistbaker.com/wp-content/uploads/2023/01/Green-bean-potato-salad-7-500x750.jpg'),
     new Recipe('A test recipe', 'this is a test', 'https://minimalistbaker.com/wp-content/uploads/2023/01/Green-bean-potato-salad-7-500x750.jpg')
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
